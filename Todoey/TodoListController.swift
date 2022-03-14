7bd965c0e38ea0c2651b083f31d9e328021d8e80
dolinkue <dolinkue_n@me.com>
2022-03-14 16:44:30 -0300

    popup

--- Todoey/TodoListController.swift
+++ Todoey/TodoListController.swift
@@ -17,6 +17,8 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    // MARK - Tableview DataSource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -48,6 +50,42 @@ class TodoListViewController: UITableViewController {
        
     }
 
+    
+    @IBAction func addBottonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        // popup para cargar items a la lista
+        let alert = UIAlertController(title: "add new Todoey list", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            // lo que pasa cuando el user da click para agregar, agrego al array
+            self.itemArray.append(textField.text!)
+            
+            // para que se cargue en la lista le debo avisar a la app
+            
+            self.tableView.reloadData()
+            
+            
+        }
+        //el espacio para cargar el texto del nuevo item
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder = "create new item"
+            textField = alertTextfield
+            
+        }
+        
+        alert.addAction(action)
+        
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+        }
+        
+    
+    
+    
 }
 
 
